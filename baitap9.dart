9befeeb70c78f6aed6702134ffc3bc99f5257e78
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-11-24 18:02:53 +0700

    đá

---
+++ baitap9.dart
@@ -0,0 +1,10 @@
+/* cho một dan sách listInt=[1,2,4,5,-1,3-3,7,8,2,-3-1,0]
+Tính trung bình cộng các số lẻ ở vị trí chẵn
+Tìm số lớn nhất trong mảng vừa nhập
+Tìm vị trí các số nhỏ nhất trong mảng
+Đếm các số chính phương có trong mảng
+Hiện thị các số nguyên tố có trong mảng lên màn hình
+Thay thế các phần tử âm có trong mảng bằng giá trị 0
+Xóa các phần tử âm có trong mảng
+Sắp xếp mảng đã nhập theo thứ tự  tăng dần và giảm dần 
+*/
