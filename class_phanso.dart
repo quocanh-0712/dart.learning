cc5d9333e1c6fe4fe29eaec62d63c28863a3bbdc
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-11-28 16:06:15 +0700

    phanso

---
+++ class_phanso.dart
@@ -0,0 +1,31 @@
+/*
+Bài 1: Viết chương trình tính cộng, trừ, nhân, chia hai phân số.
+input: 1/2 + 1/4 
+output:3/4
+*/
+class Phanso {
+  late int a;
+  late int b;
+
+  Phanso({required int a, required int b}) {
+    this.a = a;
+    this.b = b;
+  }
+
+  static cong(Phanso p1, Phanso p2) {
+    int a, b;
+    a = p1.a * p2.b + p1.b * p2.a; // tu so 
+    b = p1.b * p2.b; // mau so 
+    show(a, b);
+  }
+
+  static show(int a, int b) {
+    print('$a' + '/' '$b');
+  }
+}
+
+void main() {
+  var p1 = Phanso(a: 1, b: 2);
+  var p2 = Phanso(a: 1, b: 4);
+  Phanso.cong(p1, p2);
+}
