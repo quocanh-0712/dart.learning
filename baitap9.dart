2d0cca5ea68f2baa937432858d98cbe183d4f284
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-11-28 16:19:36 +0700

    sacas

--- baitap9.dart
+++ baitap9.dart
@@ -1,10 +1,22 @@
 /* cho một dan sách listInt=[1,2,4,5,-1,3-3,7,8,2,-3-1,0]
-Tính trung bình cộng các số lẻ ở vị trí chẵn
+Tính trung bình cộng các số lẻ ở vị trí chẵn [1 ,2 ,3, 4 ]
+-vi trí 0 thì là giá trị là: 1
+-vi trí 1 thì là giá trị là: 2
+-vi trí 2 thì là giá trị là: 3
+-vi trí 3 thì là giá trị là: 4
+Kiểm tra vị chẵn trước : là 0 với 2 
+Sau đó em kiểm tra giá trị đó là lẻ : 1 với 3 thoả mãn 
+dem++
+Vậy sum =1+3/dem=4;
 Tìm số lớn nhất trong mảng vừa nhập
 Tìm vị trí các số nhỏ nhất trong mảng
-Đếm các số chính phương có trong mảng
 Hiện thị các số nguyên tố có trong mảng lên màn hình
 Thay thế các phần tử âm có trong mảng bằng giá trị 0
-Xóa các phần tử âm có trong mảng
+Kiem tra giá trị âm thì gán listint[i]=0;
+listInt=[1,-2,3]
+[1,0,3]
+for(int i=0;i<n;i++){
+  
+}
 Sắp xếp mảng đã nhập theo thứ tự  tăng dần và giảm dần 
 */
