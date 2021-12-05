6404048a222767ab0f6750e97e5189f285bbb5cd
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-12-05 15:19:50 +0700

    Update class_phanso.dart

--- class_phanso.dart
+++ class_phanso.dart
@@ -4,8 +4,17 @@ input: 1/2 + 1/4
 output:3/4
 */
 class Phanso {
-  late int a;
-  late int b;
+  late int _a;
+  late int _b;
+
+  int get a => _a;
+  set a(int a) {
+    _a = a;
+  }
+   int get b => _b;
+  set b(int b) {
+    _b = b;
+  }
 
   Phanso({required int a, required int b}) {
     this.a = a;
@@ -14,8 +23,8 @@ class Phanso {
 
   static cong(Phanso p1, Phanso p2) {
     int a, b;
-    a = p1.a * p2.b + p1.b * p2.a; // tu so 
-    b = p1.b * p2.b; // mau so 
+    a = p1.a * p2.b + p1.b * p2.a; // tu so
+    b = p1.b * p2.b; // mau so
     show(a, b);
   }
 
